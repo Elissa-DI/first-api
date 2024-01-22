34c3e8ac8692ccbab2f33b8473013b580e84b295
Elissa-DI <elissadusabe@gmail.com>
2024-01-22 20:47:41 +0200

    new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [ AuthModule, UserModule, BookmarkModule ],
+  imports: [],
 })
-export class AppModule { }
+export class AppModule {}
